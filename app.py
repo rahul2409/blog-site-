58ddac1d147e909532a0fe9629f454ee8be787a1
rahul2409 <37111352+rahul2409@users.noreply.github.com>
2018-11-24 11:47:30 +0530

    Add files via upload
    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask , render_template
+from data import Articles
+
+
+app= Flask(__name__)
+articles = Articles()
+
+
+@app.route('/')
+def index():
+	return render_template('home.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/posts')
+def posts():
+	return render_template('posts.html', articles)
+
+if __name__ == '__main__' :
+	app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [
+        {
+            'id':1 ,
+            'title':'blog 1 ',
+            'content' : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta consectetur itaque inventore, deleniti hic dolorem quisquam voluptate numquam minus iusto quibusdam suscipit error provident molestiae voluptatibus! Nemo rerum beatae eos amet corrupti reprehenderit repellat sunt consequuntur nesciunt debitis cupiditate nihil ratione enim, odio facere. Officia ipsam amet sunt voluptates iste cumque, assumenda explicabo sequi voluptatum aut culpa nostrum, adipisci, ullam fuga sed asperiores non molestiae? Labore, iusto! Eligendi expedita eaque consequuntur amet earum adipisci nobis iusto aspernatur dolore dolores numquam sunt dignissimos accusantium est cupiditate accusamus harum illum, neque fuga blanditiis! Quisquam esse possimus, ipsum inventore perferendis voluptatibus aperiam maiores?',
+            'author':'rahul',
+            'date-of-creation':'24-09-2018'
+        },
+        {
+            'id':1 ,
+            'title':'blog 1 ',
+            'content' : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta consectetur itaque inventore, deleniti hic dolorem quisquam voluptate numquam minus iusto quibusdam suscipit error provident molestiae voluptatibus! Nemo rerum beatae eos amet corrupti reprehenderit repellat sunt consequuntur nesciunt debitis cupiditate nihil ratione enim, odio facere. Officia ipsam amet sunt voluptates iste cumque, assumenda explicabo sequi voluptatum aut culpa nostrum, adipisci, ullam fuga sed asperiores non molestiae? Labore, iusto! Eligendi expedita eaque consequuntur amet earum adipisci nobis iusto aspernatur dolore dolores numquam sunt dignissimos accusantium est cupiditate accusamus harum illum, neque fuga blanditiis! Quisquam esse possimus, ipsum inventore perferendis voluptatibus aperiam maiores?',
+            'author':'rahul',
+            'date-of-creation':'24-09-2018'
+        },
+        {
+            'id':1 ,
+            'title':'blog 1 ',
+            'content' : 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dicta consectetur itaque inventore, deleniti hic dolorem quisquam voluptate numquam minus iusto quibusdam suscipit error provident molestiae voluptatibus! Nemo rerum beatae eos amet corrupti reprehenderit repellat sunt consequuntur nesciunt debitis cupiditate nihil ratione enim, odio facere. Officia ipsam amet sunt voluptates iste cumque, assumenda explicabo sequi voluptatum aut culpa nostrum, adipisci, ullam fuga sed asperiores non molestiae? Labore, iusto! Eligendi expedita eaque consequuntur amet earum adipisci nobis iusto aspernatur dolore dolores numquam sunt dignissimos accusantium est cupiditate accusamus harum illum, neque fuga blanditiis! Quisquam esse possimus, ipsum inventore perferendis voluptatibus aperiam maiores?',
+            'author':'rahul',
+            'date-of-creation':'24-09-2018'
+        }
+    ];
+    return articles;
+    
