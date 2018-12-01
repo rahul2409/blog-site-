f602300c396aea6f7d5e656a6c450b2d52b83fb8
Rahul Nandrajog <2016.rahul.nandrajog@ves.ac.in>
2018-12-01 09:59:40 +0530

    login intialised without the .html file

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ from passlib.hash import sha256_crypt
 
 
 app= Flask(__name__)
+
+#configure mysql 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'blog_site'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+
+# initialize sql 
+mysql = MySQL(app)
+
+
 articles = Articles()
 
 
@@ -34,7 +47,53 @@ class Registration(Form):
 def register():
 	form = Registration(request.form)
 	if request.method == 'POST' and form.validate():
-		return render_template('register.html')
+		name = form.name.data
+		email=form.email.data
+		username = form.username.data 
+		password = sha256_crypt.encrypt(str(form.password.data))
+
+		#initializing server 
+		cursor = mysql.connection.cursor()
+
+		#execute query 
+		cursor.execute("insert into users (name,email,username,password) values(%s,%s,%s,%s)",(name,email,username,password))
+
+		# commiting to db 
+		mysql.connection.commit()
+
+		# close connection 
+		cursor.close()
+
+		flash('you are now registered and can login ','success')
+
+		return redirect(url_for('index'))
 	return render_template('register.html',form=form)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		username = request.form['username']
+		candidate_password = request.form['password']
+
+		#create cursor 
+		cursor = mysql.connection.cursor()
+
+		#fire query
+		result = cursor.execute("select * from users where username =%s ",(username))
+
+		#check the result 
+		if (result > 0):
+			data = cursor.fetchone()
+			password = data['password']
+
+			#comparing the passwords 
+			if (sha256_crypt.verify(candidate_password,password)):
+				app.logger.info('password matched ')
+			else :
+				app.logger.info('password not matched ')
+		else :
+			app.logger.info('no username')
+	return render_template('login.html')
+
 if __name__ == '__main__' :
+	app.secret_key='mrrobo123'
 	app.run(debug=True)
